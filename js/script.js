b57fa5d08080013c0254c52c1a8a66570466ec14
AkumaCipher <petitmamass59@gmail.com>
2022-10-14 18:07:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
   $("#voila").click(function () {
     var ville = $("#ville").val();
-    "Il fait " +
-      $.get(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          ville +
-          "&appid=659496a5ec528c334c3663ea0927fdd9",
-        function (data) {
-          $(".res").text(
-            "Il fait " + Math.round(data.main.temp - 273.15) + "°C à " + ville
-          );
-        }
+    var request = $.get(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        ville +
+        "&appid=659496a5ec528c334c3663ea0927fdd9"
+    );
+    request.done(function (data) {
+      $(".res").text(
+        "Il fait " + Math.round(data.main.temp - 273.15) + "°C à " + ville
       );
+    });
+    request.fail(function (data){
+        $(".res").text("La ville " + ville + " n'existe pas.");
+        console.log(data);
+    })
   });
 });
