70d9266a4b93f10d987071882e041dd3de3d939f
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2022-10-14 16:30:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-    $(bouton).click(function() {
-        
-       });
+    $("#voila").click(function() {
+        $.get('https://api.openweathermap.org/data/2.5/weather?q=' + $("#inpt").val() +'&appid=659496a5ec528c334c3663ea0927fdd9', function(data){
+            $(".rep").attr()
+            });
+        });
 });
