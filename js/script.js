8a303397dd0ec0e25165be5ce693546f86c5fe2f
PheraxLemitige <97298319+PheraxLemitige@users.noreply.github.com>
2022-10-14 16:47:30 +0200

    feat: Température
    Ajout d'une fonction permettant de trouver et afficher la température d'une ville.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $("#voila").click(function() {
-        $.get('https://api.openweathermap.org/data/2.5/weather?q=' + $("#inpt").val() +'&appid=659496a5ec528c334c3663ea0927fdd9', function(data){
-            $(".rep").attr()
+        var ville = $("#ville").val()
+        $.get('https://api.openweathermap.org/data/2.5/weather?q=' + ville +'&appid=659496a5ec528c334c3663ea0927fdd9', function(data){
+            $( '.res' ).text( "Il fait " + (Math.round(data.main.temp - 273.15)) + "°C à " + ville)
             });
         });
 });
